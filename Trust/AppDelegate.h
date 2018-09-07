e5420a904e03220cd0c445433b818ea4f0fa521c
JoniusLi <LTC@lijoniusdemacbook-pro.local>
2018-09-07 08:20:09 +0800

    Initial Commit

---
+++ Trust/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Trust
+//
+//  Created by LiJonius on 2018/9/7.
+//  Copyright © 2018年 LiJonius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Trust/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Trust
+//
+//  Created by LiJonius on 2018/9/7.
+//  Copyright © 2018年 LiJonius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
